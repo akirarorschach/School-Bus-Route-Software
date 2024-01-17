ae3d492441edca1d0905da86e7a6014a39d07fbf
Akira Rorschach <69493313+akirarorschach@users.noreply.github.com>
2024-01-16 21:13:16 -0500

    login screen basics
    https://holyswift.app/how-to-create-a-login-screen-in-swiftui/

--- School Bus Route Software/School_Bus_Route_SoftwareApp.swift
+++ School Bus Route Software/School_Bus_Route_SoftwareApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct School_Bus_Route_SoftwareApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
