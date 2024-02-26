6cb860885db37dc6fd6e26543e4ff6612bbe80db
Akira Rorschach <69493313+akirarorschach@users.noreply.github.com>
2024-02-25 22:47:03 -0500

    add mapkit capabilities
    https://github.com/akirarorschach/School-Bus-Route-Software/issues/8

---
+++ School Bus Route Software/Administrator/routeMgmt.swift
@@ -0,0 +1,20 @@
+//
+//  routeMgmt.swift
+//  School Bus Route Software
+//
+//  Created by Alvin Wu on 2024-02-25.
+//
+
+import SwiftUI
+
+struct routeMgmt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct routeMgmt_Previews: PreviewProvider {
+    static var previews: some View {
+        routeMgmt()
+    }
+}
